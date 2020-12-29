7ba873b46136147e92f3861260c7a0a0761463b4
k-r-3 <rke19922802@gmail.com>
2020-12-29 13:35:12 +0500

    Вызов метода с аргументами. [#433765]

--- src/main/java/ru/job4j/oop/Jukebox.java
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -3,10 +3,9 @@ package ru.job4j.oop;
 import java.util.Random;
 
 public class Jukebox {
-    static Random rand = new Random(47);
-
     public static void main(String[] args) {
-    Jukebox sony = new Jukebox();
+        Random rand = new Random(47);
+        Jukebox sony = new Jukebox();
         for (int i = 0; i < 6; i++) {
             sony.music(rand.nextInt(4));
         }
