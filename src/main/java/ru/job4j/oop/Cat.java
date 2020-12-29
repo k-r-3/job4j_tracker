100a89a76e88322403d40d3b861cf52498ed2635
k-r-3 <rke19922802@gmail.com>
2020-12-29 19:41:19 +0500

    Поля объекта [#433763]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,8 +1,30 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String name;
+
+    private String food;
+
+    public void giveNik(String nik) {
+        this.name = nik;
+    }
+
+    public void eat(String food) {
+        this.food = food;
+    }
+
+    public void show() {
+        System.out.println(name + " съел " + food);
+    }
+
     public static void main(String[] args) {
         Cat peppy = new Cat();
         Cat sparky = new Cat();
+        sparky.giveNik("Sparky");
+        sparky.eat("Kitekat");
+        sparky.show();
+        peppy.giveNik("Peppy");
+        peppy.eat("Whiskas");
+        peppy.show();
     }
 }
