b17d554d2047b31590a7f28c5d5637af6ac8f404
k-r-3 <rke19922802@gmail.com>
2021-01-06 17:14:39 +0500

    Что такое инкапсуляция. [#433792]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,8 +5,10 @@ public class StartUI {
 
     public static void main(String[] args) {
         Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String currentDateTime = formatter.format(item.getCreated());
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTime = DateTimeFormatter
+                .ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss")
+                .format(item.getCreated());
         System.out.println(currentDateTime);
 
     }
