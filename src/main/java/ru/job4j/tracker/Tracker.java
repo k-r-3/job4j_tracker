440369f8da47cdb878a260784ec71548bc4c524b
k-r-3 <rke19922802@gmail.com>
2021-01-06 23:40:53 +0500

    Метод удаления заявки Tracker.delete [#433787]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -57,4 +57,17 @@ public class Tracker {
         items[index] = new Item(id, item.getName());
         return true;
     }
+
+    public boolean delete (int id) {
+        int index = indexOf(id);
+        if (index == -1) {
+            return false;
+        }
+        int start = index + 1;
+        int length = size - index;
+        System.arraycopy(items, start, items, index, length);
+        items[size - 1] = null;
+        size--;
+        return true;
+    }
 }
