3b6bfc20cd92331825fb9d0315b85edc87138586
k-r-3 <rke19922802@gmail.com>
2021-01-07 11:43:50 +0500

    Tracker - хранилище [#433791]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -28,14 +28,7 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] notNullItems = new Item[size];
-        int count = 0;
-        for (int i = 0; i < size; i++) {
-            if (items[i] != null) {
-                notNullItems[count++] = items[i];
-            }
-        }
-       return Arrays.copyOf(notNullItems, count);
+       return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {
