3bd94ffdb3916d1dd4d1a67cc1a58e471fb86386
k-r-3 <rke19922802@gmail.com>
2021-01-07 17:16:05 +0500

    toString [#433790]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,19 +7,7 @@ import java.util.Arrays;
 public class StartUI {
 
     public static void main(String[] args) {
-        Item[] items = new Item[4];
-        for (int i = 0; i < 4; i++) {
-            items[i] = new Item(i, "# " + i);
-        }
-        String currentDateTime = DateTimeFormatter
-                .ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss")
-                .format(items[0].getCreated());
-        System.out.println(currentDateTime);
-        Tracker tracker = new Tracker();
-        for (Item item : items) {
-            tracker.add(item);
-        }
-        System.out.println(tracker.findById(3));
-        System.out.println(Arrays.toString(tracker.findByName("# 2")));
+        Item item = new Item(1, "first item");
+        System.out.println(item);
     }
 }
