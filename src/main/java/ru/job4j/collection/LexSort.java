3616791c21f1f83f3b99c26fb9c8d57c99276e12
k-r-3 <rke19922802@gmail.com>
2021-01-21 01:40:25 +0500

    Сортировка номера [#433685]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -6,20 +6,7 @@ public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        StringBuilder ls = new StringBuilder();
-        StringBuilder rs = new StringBuilder();
-        int size = 0;
-        int index = 0;
-        while (size < 2) {
-            if (left.charAt(index) != '.') {
-                ls.append(left.charAt(index));
-            }
-            if (right.charAt(index) != '.') {
-                rs.append(right.charAt(index));
-            }
-            index++;
-            size++;
-        }
-        return Integer.compare(Integer.parseInt(ls.toString()), Integer.parseInt(rs.toString()));
+        return Integer.compare(Integer.parseInt(left.split("\\.")[0]),
+                Integer.parseInt(right.split("\\.")[0]));
     }
 }
