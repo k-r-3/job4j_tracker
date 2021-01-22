6f153217fc58f7d739be902364ed7eabd512fd49
k-r-3 <rke19922802@gmail.com>
2021-01-22 18:01:24 +0500

    Что такое JavaDoc [#433677]

--- src/main/java/ru/job4j/bank/Account.java
+++ src/main/java/ru/job4j/bank/Account.java
@@ -2,31 +2,62 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс представляет из себя модель данных, описывающую банковский счет
+ * @autor Kozlov Roman
+ * @version 1.0
+ */
 public class Account {
     private String requisite;
     private double balance;
 
+    /**
+     * Инициалиирует экземпляр класса
+     * @param requisite - уникальный идентификатор счета
+     * @param balance - сумма средств, доступных по текущему счету
+     */
     public Account(String requisite, double balance) {
         this.requisite = requisite;
         this.balance = balance;
     }
 
+    /**
+     * Позволяет получть значение requisite
+     * @return - Возвращает значение requisite
+     */
     public String getRequisite() {
         return requisite;
     }
 
+    /**
+     * Позволяет устанавить значение requisite
+     * @param requisite - Устанавливает значение requisite
+     */
     public void setRequisite(String requisite) {
         this.requisite = requisite;
     }
 
+    /**
+     * Позволяет получить значение balance
+     * @return - Возвращает значение balance
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Позволяет устанавить значение balance
+     * @param balance - Устанавливает значение balance
+     */
     public void setBalance(double balance) {
         this.balance = balance;
     }
 
+    /**
+     * Проверяет равенство текущего объекта и поля requisite между объектом {@param o} и полем o.requisite
+     * @param o - объект , с которым проверяется равенство
+     * @return - {@code true} если объекты равны
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -39,6 +70,10 @@ public class Account {
         return Objects.equals(requisite, account.requisite);
     }
 
+    /**
+     * Создает хэш код объекта на основе поля requisite
+     * @return возвращает созданный хэш код
+     */
     @Override
     public int hashCode() {
         return Objects.hash(requisite);
--- src/main/java/ru/job4j/bank/User.java
+++ src/main/java/ru/job4j/bank/User.java
@@ -2,31 +2,62 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ *  Класс предтавляет из себя модель данных, описывающую пользователя
+ * @author Kozlov Roman
+ * @version 1.0
+ */
 public class User {
     private String passport;
     private String username;
 
+    /**
+     * Инициализирует экземляр класса
+     * @param passport - Уникальный идентификатор пользователя
+     * @param username - Имя пользователя. Не уникально
+     */
     public User(String passport, String username) {
         this.passport = passport;
         this.username = username;
     }
 
+    /**
+     * Позволяет получить значение passport
+     * @return - возвращает значение passport
+     */
     public String getPassport() {
         return passport;
     }
 
+    /**
+     * Позовляет задать значение passport
+     * @param passport - Устанавливает значение passport
+     */
     public void setPassport(String passport) {
         this.passport = passport;
     }
 
+    /**
+     * Позволяет получить значение username
+     * @return - Возвращает значение username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Позовляет задать значение username
+     * @param username - Устанавливает значение username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Проверяет равенство текущего объекта и поля passport между объектом {@param o} и полем o.passport
+     * @param o - объект , с которым проверяется равенство
+     * @return -  {@code true} в случе если объекты равны
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -39,6 +70,10 @@ public class User {
         return Objects.equals(passport, user.passport);
     }
 
+    /**
+     * Создает хэш код объекта на основе поля passport
+     * @return возвращает созданный хэш код
+     */
     @Override
     public int hashCode() {
         return Objects.hash(passport);
