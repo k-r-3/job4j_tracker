6ac152cdd969ce1104a27818b02092ea165af42a
k-r-3 <rke19922802@gmail.com>
2021-01-26 23:03:16 +0500

    Встроенные функциональные интерфейсы. [#433516]

--- src/main/java/ru/job4j/lambda/SearchAtt.java
+++ src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -1,40 +1,52 @@
 package ru.job4j.lambda;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.function.BiPredicate;
+import java.util.function.Predicate;
 
 public class SearchAtt {
 
-    public static List<Attachment> filter(List<Attachment> list, int value) {
+    public static List<Attachment> filter(List<Attachment> list, Predicate<Attachment> cond) {
         List<Attachment> rsl = new ArrayList<>();
-        BiPredicate<Attachment, Integer> func = new BiPredicate<Attachment, Integer>() {
-            @Override
-            public boolean test(Attachment att, Integer integer) {
-                return att.getSize() == integer;
-            }
-        };
         for (Attachment att : list) {
-            if (func.test(att, value)) {
+            if (cond.test(att)) {
                 rsl.add(att);
             }
         }
         return rsl;
     }
 
-    public static List<Attachment> filter(List<Attachment> list, String value) {
-        List<Attachment> rsl = new ArrayList<>();
-        BiPredicate<Attachment, String> func = new BiPredicate<Attachment, String>() {
+    public static Predicate<Attachment> filterName(Attachment att) {
+        Predicate<Attachment> predicateName = new Predicate<Attachment>() {
             @Override
-            public boolean test(Attachment att, String s) {
-                return att.getName().contains(s);
+            public boolean test(Attachment attachment) {
+                return attachment.getName().contains(att.getName());
             }
         };
-        for (Attachment att : list) {
-            if (func.test(att, value)) {
-                rsl.add(att);
+        return predicateName;
+    }
+
+        public static Predicate<Attachment> filterSize(Attachment att) {
+            Predicate<Attachment> predicateSize = new Predicate<Attachment>() {
+            @Override
+            public boolean test(Attachment attachment) {
+                return attachment.getSize() > att.getSize();
             }
-        }
-        return rsl;
+        };
+        return predicateSize;
+    }
+
+    public static void main(String[] args) {
+        Attachment attSize = new Attachment("", 100);
+        Attachment attName = new Attachment("bug", 0);
+        List<Attachment> la = Arrays.asList(
+                new Attachment("image 1", 100),
+                new Attachment("image 2", 134),
+                new Attachment("image 3", 13),
+                new Attachment("bugs", 134)
+        );
+        List<Attachment> filterAttachment =  filter(la, filterSize(attSize));
+        filterAttachment = List.copyOf(filter(filterAttachment, filterName(attName)));
     }
 }
