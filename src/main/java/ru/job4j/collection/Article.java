50a7d929b78167ae4c99cde14c8f50166bd98016
k-r-3 <rke19922802@gmail.com>
2021-01-27 21:37:09 +0500

    Экзамен. Коллекции. [#433682]

--- src/main/java/ru/job4j/collection/Article.java
+++ src/main/java/ru/job4j/collection/Article.java
@@ -3,46 +3,31 @@ package ru.job4j.collection;
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class Article {
     public static boolean generateBy(String origin, String line) {
+        Map<String, Integer> map = new HashMap<>();
+        int amount = 0;
         origin = origin.toUpperCase();
         line = line.toUpperCase();
-        Map<String, Integer> map = new HashMap<>();
         String[] lineArr = line.split(" ");
-        for (String o : origin.split(" ")) {
-            if (map.containsKey(o)) {
-                map.put(o, map.get(o) + 1);
+        String[] originArr = origin.split(" ");
+        for (String o : originArr) {
+            String word = o.replaceAll("[,!.]", "");
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+            } else {
+                map.put(word, 1);
             }
-            map.put(o, 1);
         }
-        int startSize = map.size();
-        for (String l : line.split(" ")) {
+        for (String l : lineArr) {
             if (map.containsKey(l)) {
                 map.put(l, map.get(l) - 1);
+                amount++;
                 if (map.get(l) == 0) {
                     map.remove(l);
                 }
             }
-            if (map.containsKey(l + ",")) {
-                map.put(l + ",", map.get(l + ",") - 1);
-                if (map.get(l + ",") == 0) {
-                    map.remove(l + ",");
-                }
-            }
-            if (map.containsKey(l + ".")) {
-                map.put(l + ".", map.get(l + ".") - 1);
-                if (map.get(l + ".") == 0) {
-                    map.remove(l + ".");
-                }
-            }
-            if (map.containsKey(l + "!")) {
-                map.put(l + "!", map.get(l + "!") - 1);
-                if (map.get(l + "!") == 0) {
-                    map.remove(l + "!");
-                }
-            }
         }
-        return map.size() == startSize - lineArr.length;
+        return amount == lineArr.length;
     }
 }
