8ff25e58c8e27d6bdf30cf113ed638bd89e5649f
k-r-3 <rke19922802@gmail.com>
2021-01-28 18:30:40 +0500

    Лямбда блок [#433515]

---
+++ src/main/java/ru/job4j/lambda/LambdaBlock.java
@@ -0,0 +1,10 @@
+package ru.job4j.lambda;
+
+import java.util.Comparator;
+
+public class LambdaBlock {
+    Comparator<String> comparator = (left, right) -> {
+        System.out.println("compare " + left.length() + " : " +  right.length());
+        return Integer.compare(right.length(), left.length());
+    };
+}
