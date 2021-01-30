58b5fa6a36d8f540fc497a6540af304fc2177f2f
k-r-3 <rke19922802@gmail.com>
2021-01-30 17:36:29 +0500

    Преобразование матрицы чисел в список чисел. [#433527]

--- src/main/java/ru/job4j/stream/FlattingMap.java
+++ src/main/java/ru/job4j/stream/FlattingMap.java
@@ -1,20 +1,16 @@
 package ru.job4j.stream;
 
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FlattingMap {
-    Integer[][] example = {{3,4},
-            {5,6}
-    };
-
+    Integer[][] example = {{3,4}, {5,6}};
 
     public List<Integer> flatting(Integer[][] input) {
-        return List.of(input).stream()
-                .flatMap(i -> List.of(i)
-                        .stream())
+        return Stream.of(input)
+                .flatMap(Stream::of)
                 .collect(Collectors.toList());
     }
+
 }
