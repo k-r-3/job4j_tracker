da8e65f52b65bba1648a072bf94cf540ad9c954e
k-r-3 <rke19922802@gmail.com>
2021-01-30 21:30:44 +0500

    Шаблоны проектирования в Stream API [#433531]

---
+++ src/main/java/ru/job4j/stream/builtObject/BuilderRun.java
@@ -0,0 +1,15 @@
+package ru.job4j.stream.builtObject;
+
+public class BuilderRun {
+    public static void main(String[] args) {
+        Pojo pojo = new Pojo.Builder().buildName("Ivan")
+                .buildSurname("Ivanov")
+                .buildAge((short) 32)
+                .buildMarried(false)
+                .buildSalary((short) 1000)
+                .setPosition("developer")
+                .build();
+        System.out.println(pojo);
+
+    }
+}
---
+++ src/main/java/ru/job4j/stream/builtObject/Pojo.java
@@ -0,0 +1,73 @@
+package ru.job4j.stream.builtObject;
+
+public class Pojo {
+    private String name;
+    private String surname;
+    private short age;
+    private boolean isMarried;
+    private short salary;
+    private String position;
+
+    static class Builder{
+        private String name;
+        private String surname;
+        private short age;
+        private boolean isMarried;
+        private short salary;
+        private String position;
+
+        public Builder buildName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder buildSurname(String surname) {
+            this.surname = surname;
+            return this;
+        }
+
+        public Builder buildAge(short age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder buildMarried(boolean married) {
+            isMarried = married;
+            return this;
+        }
+
+        public Builder buildSalary(short salary) {
+            this.salary = salary;
+            return this;
+
+        }
+
+        public Builder setPosition(String position) {
+            this.position = position;
+            return this;
+        }
+
+         Pojo build() {
+            Pojo pojo = new Pojo();
+            pojo.name = name;
+            pojo.surname = surname;
+            pojo.age = age;
+            pojo.isMarried = isMarried;
+            pojo.salary = salary;
+            pojo.position = position;
+            return pojo;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Pojo{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", isMarried=" + isMarried +
+                ", salary=" + salary +
+                ", position='" + position + '\'' +
+                '}';
+    }
+}
