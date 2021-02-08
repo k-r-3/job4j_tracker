8b2fc106c6a9c6632a0a9d8522691418f73c8301
k-r-3 <rke19922802@gmail.com>
2021-02-08 15:05:51 +0500

    EasyStream2 [#401629]

--- src/main/java/ru/job4j/stream/builtobject/BuilderRun.java
+++ src/main/java/ru/job4j/stream/builtobject/BuilderRun.java
@@ -4,7 +4,6 @@ public class BuilderRun {
     public static void main(String[] args) {
         Pojo pojo = new Pojo.Builder().buildName("Ivan")
                 .buildSurname("Ivanov")
-                .buildAge((short) 32)
                 .buildMarried(false)
                 .buildSalary((short) 1000)
                 .setPosition("developer")
--- src/main/java/ru/job4j/stream/simplestream/EasyStream.java
+++ src/main/java/ru/job4j/stream/simplestream/EasyStream.java
@@ -6,32 +6,53 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class EasyStream {
-     private static List<Integer> domainList;
+
+    static class InnerFeature {
+         private static List<Integer> innerList;
+
+         private static List<Integer> getInnerList() {
+             return innerList;
+         }
+
+         private static void setInnerList(List<Integer> list) {
+             innerList = list;
+         }
+
+         private static void map(Function<Integer, Integer> fun) {
+             var mappedList = new ArrayList<Integer>();
+             for (Integer i : innerList) {
+                 mappedList.add(fun.apply(i));
+             }
+             setInnerList(mappedList);
+         }
+
+         private static void filter(Predicate<Integer> fun) {
+             var filteredList = new ArrayList<Integer>();
+             for (Integer i : innerList) {
+                 if (fun.test(i)) {
+                     filteredList.add(i);
+                 }
+             }
+             setInnerList(filteredList);
+         }
+     }
 
     public static EasyStream of(List<Integer> source) {
-        domainList = new ArrayList<>(source);
+        InnerFeature.setInnerList(source);
         return new EasyStream();
     }
 
-    public synchronized EasyStream map(Function<Integer, Integer> fun) {
-        for (int i = 0; i < domainList.size(); i++) {
-            domainList.set(i, fun.apply(domainList.get(i)));
-        }
+    public EasyStream map(Function<Integer, Integer> fun) {
+        InnerFeature.map(fun);
         return new EasyStream();
     }
 
-    public synchronized EasyStream filter(Predicate<Integer> fun) {
-        var filteredList = new ArrayList<Integer>();
-        for (Integer i : domainList) {
-            if (fun.test(i)) {
-                filteredList.add(i);
-            }
-        }
-        domainList = filteredList;
+    public EasyStream filter(Predicate<Integer> fun) {
+        InnerFeature.filter(fun);
         return new EasyStream();
     }
 
     public List<Integer> collect() {
-        return domainList;
+         return InnerFeature.getInnerList();
     }
 }
